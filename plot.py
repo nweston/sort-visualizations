dfe9d1f025773c23e6a91cf569810e2d308e5663
Nathan Weston <nweston@fastmail.com>
2019-02-05 08:38:33 -0500

    Function to visualize a list with scaled circles.

--- plot.py
+++ plot.py
@@ -1,4 +1,6 @@
 import matplotlib.pyplot as plt
+import matplotlib.lines as lines
+import matplotlib.patches as patches
 from matplotlib.animation import FuncAnimation
 import sort
 
@@ -44,3 +46,31 @@ def animated_bars(sort_func, data, interval):
         return bars
 
     return FuncAnimation(fig, animate, frames=len(states), interval=interval)
+
+
+def circle_plot(fig, data):
+    # Artists are scaled according to aspect ratio. We'll need to undo this to draw circles.
+    xsize, ysize = fig.get_size_inches()
+    yscale = xsize / ysize
+
+    # This magic invisible line makes the figure show up
+    fig.lines.append(lines.Line2D([0, 0], [0, 1], transform=fig.transFigure,
+                                  figure=fig, color='white'))
+
+    spacing = 1.0 / (len(data) + 1)
+    radius = 0.8 * spacing
+    smallest = min(*data)
+    largest = max(*data)
+
+    def scaled_radius(value):
+        interp = (value - smallest) / (largest - smallest)
+        return radius * (0.6 + interp * 0.4)
+
+    for i, v in enumerate(data):
+        x = (i + 0.5) * spacing
+        r = scaled_radius(v)
+        c = patches.Ellipse((x, 0.5), r, yscale * r, transform=fig.transFigure, figure=fig,
+                          fill=False)
+        fig.patches.append(c)
+
+    return fig
